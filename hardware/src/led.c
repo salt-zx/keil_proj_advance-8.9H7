ee10a009da36ce3c68707e2055d5121d5222479d
salt-zx <xbz-zwz21@qq.com>
2026-08-09 17:26:21 +0800

    遥控流水灯状态机

--- hardware/src/led.c
+++ hardware/src/led.c
@@ -24,6 +24,15 @@ typedef struct
     uint32_t off_ms;
 } LED_BlinkParam;
 
+typedef enum
+{
+    LED_MODE_SINGLE =0U,
+    LED_MODE_PAIR,
+    LED_MODE_ALL
+} LED_Mode;
+
+static LED_Mode mode = LED_MODE_SINGLE;
+
 static const uint16_t led_pin_table[LED_COUNT] =
 {
     LED1_PIN,
@@ -70,7 +79,7 @@ static void blink(const LED_BlinkParam *param)
     HAL_Delay(param->off_ms);
 }
 
-void led_flow(void)
+static void led_blink_single(void)
 {
     static const LED_BlinkParam flow[] =
     {
@@ -85,5 +94,95 @@ void led_flow(void)
     for (i = 0U; i < LED_COUNT; i++)
     {
         blink(&flow[i]);
+    }
+}
+
+static void blink_pair(const LED_BlinkParam *param1, const LED_BlinkParam *param2)
+{
+    if (param1 == 0 || param2 == 0 || !led_num_is_valid(param1->led_num) || !led_num_is_valid(param2->led_num))
+    {
+        return;
+    }
+    led_on(param1->led_num);
+    led_on(param2->led_num);
+    HAL_Delay(param1->on_ms);
+    led_off(param1->led_num);
+    led_off(param2->led_num);
+    HAL_Delay(param1->off_ms);
+}
+
+static void led_blink_pair(void)
+{
+    static const LED_BlinkParam flow[] =
+    {
+        {LED1_ID, 250U, 250U},
+        {LED2_ID, 250U, 250U},
+        {LED3_ID, 250U, 250U},
+        {LED4_ID, 250U, 250U}
+    };
+
+    uint8_t i;
+
+    for (i = 0U; i < LED_COUNT; i+=2U)
+    {
+        blink_pair(&flow[i], &flow[(i + 1U) % LED_COUNT]);
+    }
+}
+
+static void led_blink_all(void)
+{
+    static const LED_BlinkParam flow =
+    {
+        LED1_ID, 250U, 250U
+    };
+
+    uint8_t i;
+
+    for (i = 0U; i < LED_COUNT; i++)
+    {
+        led_on(i);
+    }
+    HAL_Delay(flow.on_ms);
+    for (i = 0U; i < LED_COUNT; i++)
+    {
+        led_off(i);
+    }
+    HAL_Delay(flow.off_ms);
+}
+
+void led_flow(void)
+{
+    uint8_t signal= 0U;
+
+    switch (signal)
+    {
+        case 0:
+            mode = LED_MODE_SINGLE;
+            break;
+        case 1:
+            mode = LED_MODE_PAIR;
+            break;
+        case 2:
+            mode = LED_MODE_ALL;
+            break;
+        default:
+            mode = LED_MODE_SINGLE;
+            break;
+    }
+
+    switch (mode)
+    {
+        case LED_MODE_SINGLE:
+            led_blink_single();
+            break;
+        case LED_MODE_PAIR:
+            led_blink_pair();
+            break;
+        case LED_MODE_ALL:
+            led_blink_all();
+            break;
+        default:
+            mode = LED_MODE_SINGLE;
+            break;
     }
 }
